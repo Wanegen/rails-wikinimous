b2744bf9745494191cb40f717415baf06e1617d3
Maxandre Dufour <maxandre.dufour@gmail.com>
2024-02-20 16:54:52 +0100

    Ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do
+  title = Faker::Book.title
+  content = Faker::Lorem.paragraphs(number: 3).join("\n")
+  Article.create(title: title, content: content)
+end
